138775b2215a23f064fc6a79fdf56f4189598cf5
G01\mishima.haruka <mishima.haruka@jp.fujitsu.com>
2019-01-09 17:19:25 +0900

    サイコロボタン、関数作成

--- main.py
+++ main.py
@@ -24,28 +24,43 @@ class Main:
         size = (min(settings.SCREEN_SIZE), min(settings.SCREEN_SIZE))
         img = pygame.transform.scale(img, size)
         return img
+    
+    @staticmethod
+    def __load_img_dice(path):
+        img = pygame.image.load(path).convert_alpha()
+        size = (min(settings.DICE_SIZE), min(settings.DICE_SIZE))
+        img = pygame.transform.scale(img, size)
+        return img
+
 
     def __init__(self):
         pygame.init()
         pygame.display.set_caption(settings.TITLE)
         self.__screen = pygame.display.set_mode(settings.SCREEN_SIZE)
         self.__img_background = self.__load_img_background(settings.IMG_BACKGROUND)
+        self.__img_dice = self.__load_img_dice(settings.IMG_DICE)
+        self.diceRect = pygame.Rect(settings.DICE_POS, self.__img_dice.get_rect().size)
         self.__board = Board()
-        Socket.connectServer();
+        Socket.connect_server()
 
         while not Main.__has_exit():
             self.__loop()
             pygame.time.Clock().tick(settings.FPS)
 
-        Socket.disconnectServer();
+        Socket.disconnect_server()
         pygame.quit()
         sys.exit()
 
     def __loop(self):
         self.__clear()
         self.__screen.blit(self.__img_background, (0, 0))  # Draw background
+        self.__screen.blit(self.__img_dice, settings.DICE_POS) # Draw dice
         self.__board.disp(self.__screen)
         pygame.display.update()
+        
+        if(pygame.mouse.get_pressed()[0] == 1):
+                if self.diceRect.collidepoint(pygame.mouse.get_pos()):
+                    print("push")
         self.__board.next()
 
     def __clear(self):
--- settings.py
+++ settings.py
@@ -1,11 +1,15 @@
 # General
 TITLE = "Monopoly"
 SCREEN_SIZE = (640, 480)
+DICE_SIZE = (80, 80)
+DICE_POS = (500 , 400)
+DICE_RECT = (580, 480)
 FPS = 60  # Do not set more then 60
 # FPS = 2  # Do not set more then 60
 
 # Path
 IMG_BACKGROUND = "monopoly.jpg"
+IMG_DICE = "dice.png"
 
 # Player properties
 # PLAYER_ACC = 0.5
